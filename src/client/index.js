5115cf3f4c174fab7aee1fac455d8987694108be
mahmoud <m0597731363@gmail.com>
2018-02-13 10:33:37 +0200

    start secetary view

--- src/client/index.js
+++ src/client/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import store from './store';
 import Home from './components/home';
 import DoctorsPage from './components/doctorsPage';
-import store from './store';
 import MyCalendar from './components/calender';
+import SecertaryPage from './components/secertaryPage';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -14,6 +15,8 @@ ReactDOM.render(
           <Route exact path='/' component={Home} />
           <Route exact path='/doctors/:cityName' component={DoctorsPage} />
           <Route exact path='/calender' component={MyCalendar} />
+          <Route exact path='/secertary' component={SecertaryPage} />
+
           {/* <Route exact path='/login' component={Login} /> */}
           {/* <Route path='/' component={PageNotFound} /> */}
         </Switch>
